cf19cf0a6ae85360efd9bda809105496430b164d
sepgh <13250403+sepgh@users.noreply.github.com>
2024-07-03 20:04:11 +0330

    feat: argument parsing

--- TesseraTwist/tessera_twist.py
+++ TesseraTwist/tessera_twist.py
@@ -1,37 +1,75 @@
+import argparse
+
 from image.helper import get_image_info
-from image.image_generator import VerticalSingleSourceImageGenerator
+from image.image_generator import VerticalSingleSourceImageGenerator, HorizontalSingleSourceImageGenerator
 from image.sequence_generator import SequenceGenerator
 from video.timelapse_generator import TimelapseGenerator
 
 
 if __name__ == "__main__":
-    factory = lambda: VerticalSingleSourceImageGenerator(
-        source="sample2.jpg",
-        pieces_count=20,
+
+    parser = argparse.ArgumentParser(
+        prog='Tessera Twist',
+        description='Sliced image turns into a timelaps',
+        epilog='Get more info at: https://github.com/sepgh/TesseraTwist'
     )
+    parser.add_argument('-i', '--input', help='Input image file', required=True)
+    parser.add_argument('-o', '--output', help='Output timelapse path (including filename ending in mp4)', required=True)
+    parser.add_argument(
+        '-g',
+        '--generator',
+        choices=['SV', 'SH'],
+        required=True,
+        help='Image generator type. '
+             '"SV" for "VerticalSingleSourceImageGenerator" or "SH" for "HorizontalSingleSourceImageGenerator"',
+    )
+    parser.add_argument(
+        '-s',
+        '--storage',
+        help='Storage directory to store timelapse images in. Default would be in temp.',
+        required=False
+    )
+    parser.add_argument('-p', '--pieces', help='Pieces / Slices count', type=int, default=10, required=False)
+    parser.add_argument('-w', '--workers', help='Number of worker processes', type=int, default=4, required=False)
+    parser.add_argument('-d', '--duration', help='Duration of the clip in seconds', type=int, default=15, required=False)
+    parser.add_argument('-fb', '--ffmpeg', help='Path to ffmpeg executable', type=str, default='ffmpeg', required=False)
+    parser.add_argument('-crf', help="Value for crf flag passed to ffmpeg", type=int, default=17, required=False)
+    parser.add_argument('-fps', help="Value for fps flag passed to ffmpeg", type=int, default=24, required=False)
+    args = vars(parser.parse_args())
 
-    w, h, ext = get_image_info("sample2.jpg")
+    if args['generator'] == 'SV':
+        factory = lambda: VerticalSingleSourceImageGenerator(
+            source=args["input"],
+            pieces_count=args["pieces"],
+        )
+    else:
+        factory = lambda: HorizontalSingleSourceImageGenerator(
+            source=args["input"],
+            pieces_count=args["pieces"],
+        )
 
-    seconds = 10
+    w, h, ext = get_image_info(args["input"])
+
+    seconds = args["duration"]
 
     for s in range(seconds):
         sequence_generator = SequenceGenerator(
-            sequence_length=24,
+            sequence_length=args["fps"],
             image_generator_factory=factory,
-            workers=10,
-            output_directory="./output/",
-            output_prefix=f"output.s{s}",
+            workers=args["workers"],
+            output_directory=args["storage"],
+            output_prefix=f"tessera_twist.s{s}",
             extension=ext
         )
 
         sequence_generator.generate()
 
     TimelapseGenerator(
-        "ffmpeg",
-        24,
-        ext,  # Todo: detect
-        crf=17,
-        input_directory="./output/",
-        output_path="./timelapse.mp4",
-        width=w, height=h  # Todo: detect
+        args["ffmpeg"],
+        args["fps"],
+        ext,
+        crf=args["crf"],
+        input_directory=args["storage"],
+        output_path=args["output"],
+        width=w, height=h
     ).generate()
